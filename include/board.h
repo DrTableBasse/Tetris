5e2fd33ff5263b259527f8b5f80227901bb56e2b
Nyasn <yannis.stefanelli@epitech.eu>
2023-01-10 10:49:15 +0100

    code comment and cleaning up

--- include/board.h
+++ include/board.h
@@ -13,6 +13,7 @@
 using namespace sf;
 using namespace std;
 
+//Board class definition
 class Board {
     private:
         void size(int x, int y);
@@ -21,14 +22,9 @@ class Board {
 
         char **tab;
         void setsize(int x, int y);
-        void update();
-        Vector2i pos;
-
-        Texture texture;
-        Sprite sprite;
-    
 };
 
+//board assignment in memory
 void Board::size(int x, int y)
 {
 
@@ -38,31 +34,20 @@ void Board::size(int x, int y)
         tab[i] = (char *)malloc(sizeof(char) * 10);
 }
 
+//function for declaring a board
 Board::Board(void)
 {
-    this->pos.x = 5;
-    this->pos.y = 0;
-
-    if (!this->texture.loadFromFile("../sprites/logo.jpg"))
-        cout << "Texture n'a pas chargé\n";
-    this->sprite.setTexture(this->texture);
-    this->sprite.setScale(0.1f, 0.1f);
-
+    //Setting default size
     this->size(10, 20);
     
-    
+    //remove these 2 lines, they are for debug
     this->tab[1][3] = '1';
     printf("%c\n", this->tab[0][3]);
-    this->sprite.setPosition((600 / 10) * this->pos.x, (1080 / 20) * this->pos.y);
+    //
 };
 
+//use this function if you want to resize your board
 void Board::setsize(int x, int y)
 {
     this->size(x, y);
 };
-
-void Board::update()
-{
-    pos.y += 1;
-    sprite.setPosition((600 / 10) * pos.x, (1080 / 20) * pos.y);
-}
--- include/tetromino.h
+++ include/tetromino.h
@@ -10,19 +10,7 @@
 
 class Tetromino {
     private :
-        void o_shape();
-        int size
+        int size;
     public :
-        Init(char type);
+        Tetromino(char type);
 };
-
-Tetromino::Init(char type)
-{
-
-};
-
-void o_shape() 
-{
-    size = 2;
-    height = 2;
-}
