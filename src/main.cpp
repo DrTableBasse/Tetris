13ec5169597bcf45eade20c7e56a6a84ab01896c
Nyasn <yannis.stefanelli@epitech.eu>
2023-01-10 10:57:47 +0100

    code clearing

--- src/main.cpp
+++ src/main.cpp
@@ -9,15 +9,18 @@ using namespace std;
 
 int main(void)
 {
+    //resolution and creation of window
     Vector2i res(700, 1080);
     RenderWindow window(VideoMode(res.x, res.y), "TetrESGI", Style::Titlebar | Style::Close);
+    
+    //loading icon
     auto image = sf::Image{};
     if (!image.loadFromFile("../sprites/logo.jpg"))
         cout << "Icone n'a pas chargé\n";
     window.setIcon(image.getSize().x, image.getSize().y, image.getPixelsPtr());
 
+    //declaration of the board
     Board board;
-    Clock clock;
 
     //Frame loop
     while (window.isOpen())
@@ -35,11 +38,9 @@ int main(void)
 
         //Clearing the window after each draw
         window.clear(Color(0, 0, 0));
-        if (clock.getElapsedTime().asSeconds() > 0.4f){
-            board.update();
-            clock.restart();
-        }
-        window.draw(board.sprite);
+
+        //display your sprites here
+
         window.display();
     }
 
